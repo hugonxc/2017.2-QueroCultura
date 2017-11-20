48b850a7160811f64f57b2459cfd50ec61ce3fee
Hugo Carvalho <hugo-nc@hotmail.com>
2017-11-20 15:48:10 -0200

    Change settings

--- quero_cultura/settings.py
+++ quero_cultura/settings.py
@@ -134,8 +134,11 @@ CELERY_TIMEZONE = TIME_ZONE
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-STATIC_ROOT= os.path.join(PROJECT_DIR,'static/')
-STATICFILES_DIRS = ()
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
