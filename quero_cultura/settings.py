cc43141a0bdf5aa567f3ba13bd4bfe5f09d52ca9
Hugo Carvalho <hugo-nc@hotmail.com>
2017-11-20 15:55:13 -0200

    God please make this work

--- quero_cultura/settings.py
+++ quero_cultura/settings.py
@@ -133,12 +133,4 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = TIME_ZONE
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
